21add4f4dbbc8dd78f88b9a747a6c42617a80dbd
NishantSingh135 <nishant.singh@infostretch.com>
2017-06-30 14:47:28 +0530

    checkout in progress

--- nishantpipeline.groovy
+++ nishantpipeline.groovy
@@ -1,11 +1,24 @@
+REPOSITORY_URL="https://github.com/NishantSingh135/JenkinDemo"
+
 node {
-    stage('Build') {
+
+
+stage 'checkout' 
+log('checking out source')
+checkout()
+log('checkout success')
+
+    stage'Build'
         echo 'Building....'
-    }
-    stage('Test') {
+    
+    stage 'Test' 
         echo 'Building....'
-    }
-    stage('Deploy') {
+    
+    stage 'Deploy' 
         echo 'Deploying....'
-    }
+
+        def checkout(){
+         	log ('in progress')
+        }
+    
 }
