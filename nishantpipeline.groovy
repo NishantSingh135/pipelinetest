a4086a536d78f3e82247090219ab53ceb3c52bf8
NishantSingh135 <nishant.singh@infostretch.com>
2017-06-30 18:35:14 +0530

    node:android node

--- nishantpipeline.groovy
+++ nishantpipeline.groovy
@@ -2,7 +2,7 @@ REPOSITORY_URL="https://github.com/NishantSingh135/JenkinDemo"
 // Type of build
 BUILD_TYPE = 'BUILD_DEBUG';
 
-node {
+node (android_node){
 
 stage 'checkout'  
 echo('checking out source') 
@@ -54,4 +54,5 @@ This will build the project
  	echo "current directory is :" + pwd()
  	bat "gradlew test"
  	junit '**/test-results/testDebugUnitTest/*.xml'
+ 	archiveArtifacts '**/test-results/testDebugUnitTest/*.zip'
  }   
